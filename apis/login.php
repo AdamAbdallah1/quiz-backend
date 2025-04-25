be8322bfc9718132131d0335e191fe57bc32400a
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-25 23:14:02 +0300

    Preparing sql query and bind params

--- apis/login.php
+++ apis/login.php
@@ -5,3 +5,11 @@ include '../config/config.php';
 $email = $_POST['email'] ?? '';
 $password = $_POST['password'] ?? '';
 
+if ($email && $password) {
+    $sql = "SELECT * FROM users where username = ? AND password = ?";
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bind_param("ss", $email, $password);
+
+    
+}
