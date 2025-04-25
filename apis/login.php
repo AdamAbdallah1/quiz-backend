5eb85200b036ef9ed7dcfaa3c5146d62d7440574
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-25 23:18:34 +0300

    Searching for user

--- apis/login.php
+++ apis/login.php
@@ -6,7 +6,7 @@ $email = $_POST['email'] ?? '';
 $password = $_POST['password'] ?? '';
 
 if ($email && $password) {
-    $sql = "SELECT * FROM users where username = ? AND password = ?";
+    $sql = "SELECT * FROM users where email = ? AND password = ?";
     $stmt = $conn->prepare($sql);
 
     $stmt->bind_param("ss", $email, $password);
@@ -14,4 +14,15 @@ if ($email && $password) {
     $stmt->execute();
     $result = $stmt->get_result();
     
+    if ($result->num_rows === 1){
+        $user = $result->fetch_assoc();
+        echo "Welcome User: " . $user['username'];
+    } else {
+        echo "Invalid Email or Password";
+    } 
+    $stmt->close();
+} else {
+    echo "Please enter both Email and Password!";
 }
+$conn->close();
+?>
