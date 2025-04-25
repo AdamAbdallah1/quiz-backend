7760abcdcdd8b00d8784b44abfb40ff744141a8f
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-25 23:09:38 +0300

    Fixing error and letting user to register

--- apis/register.php
+++ apis/register.php
@@ -2,16 +2,29 @@
 
 include '../config/config.php';
 
-$username = $_POST['user_name'] ?? '';
-$email = $_POST['user_email'] ?? '';
-$password = $_POST['user_password'] ?? '';
+$username = $_POST['username'] ?? '';
+$email = $_POST["email"] ?? '';
+$password = $_POST['password'] ?? '';
 
 if ($username && $email && $password) {
     $is_admin = 0;
 
-    $sql = "INSERT INTO users (username, email, password, is_admin) VALUES (?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO users (username, email, password, is_admin) VALUES (?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
 
     $stmt->bind_param("sssi", $username, $email, $password, $is_admin);
-    
+
+    if ($stmt->execute()) {
+        echo "User: ". $username . " Registered Successfully!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+
+} else {
+    echo "Please provide username, email and password.";
 }
+
+$conn->close();
+?>
