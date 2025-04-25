d873334989e7fc6dfff82b9b1b48d9b45bebb0c4
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-25 22:35:00 +0300

    Setting the sql query and reparing it

--- apis/register.php
+++ apis/register.php
@@ -6,3 +6,11 @@ $username = $_POST['user_name'] ?? '';
 $email = $_POST['user_email'] ?? '';
 $password = $_POST['user_password'] ?? '';
 
+if ($username && $email && $password) {
+    $is_admin = 0;
+
+    $sql = "INSERT INTO users (username, email, password, is_admin) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+
+
+}
