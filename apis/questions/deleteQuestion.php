a2d9fc1f23033705afc3848ad225168a160cb79d
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-26 20:06:31 +0300

    Getting question by id by admin

--- apis/questions/deleteQuestion.php
+++ apis/questions/deleteQuestion.php
@@ -10,3 +10,16 @@ if (!isset($_SESSION['user'])){
 }
 
 $user = $_SESSION['user'];
+if ($user['is_admin'] == 1){
+    $id = $_POST['id'];
+
+    if ($id){
+        $sql = "DELETE FROM questions WHERE question_id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $id);
+
+        
+    } else {
+        echo "Please provide a correct ID";
+    }
+}
