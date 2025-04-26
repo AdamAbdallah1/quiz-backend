0e1baf83bc8a9b8654b9809457683cf4a7d0d74b
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-26 16:03:11 +0300

    Getting user and setting query

--- apis/getQuizzes.php
+++ apis/getQuizzes.php
@@ -1,4 +1,25 @@
 <?php
 
+session_start();
+
 include '../config/config.php';
 
+if (isset($_SESSION['user'])) {
+    $user = $_SESSION['user'];
+    if ($user['id'] > 0){
+        echo "Welcome User: " . $user['username'];
+
+        $sql = "SELECT * FROM quizzes";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        
+
+    } else {
+        echo "Invalid User Id";
+    }
+} else {
+    echo "No user logged in";
+}
+
+?>
