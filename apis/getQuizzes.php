c1642ddb70fe21d0033e1c4c11763eb055926f76
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-26 17:20:00 +0300

    Cleaning code

--- apis/getQuizzes.php
+++ apis/getQuizzes.php
@@ -1,6 +1,7 @@
 <?php
 
 session_start();
+header("Content-Type: text/html");
 
 include '../config/config.php';
 
@@ -13,7 +14,18 @@ if (isset($_SESSION['user'])) {
         $stmt = $conn->prepare($sql);
         $stmt->execute();
 
-        
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            while ($quiz = $result->fetch_assoc()) {
+                echo "\nQuizz Title: " . $quiz['title'] . " \n";
+                echo "Description: " . $quiz['description'] . "\n";
+                echo "Owner: " . $_SESSION['user']['username'] . " \n\n";
+            }
+        } else {
+            echo "No quizzes found";
+        }
+        $stmt->close();
 
     } else {
         echo "Invalid User Id";
@@ -21,5 +33,5 @@ if (isset($_SESSION['user'])) {
 } else {
     echo "No user logged in";
 }
-
+$conn->close();
 ?>
