86e406068b84aa53d6d5c5254993226256f8917b
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-26 17:45:53 +0300

    Getting id and data from user

--- apis/editQuiz.php
+++ apis/editQuiz.php
@@ -3,3 +3,16 @@ session_start();
 
 include '../config/config.php';
 
+if (isset($_SESSION['user'])) {
+    $user = $_SESSION['user'];
+    if ($user['id'] > 0) {
+        echo "Welcome User: " . $user['username'] . "\n";
+        
+        $id = $_POST['id'] ?? 0;
+        $new_title = $_POST['title'] ?? '';
+        $new_description = $_POST['description'] ?? '';
+
+        
+
+    }
+}
