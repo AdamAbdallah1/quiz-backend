20e877e850d03e5823351d963baf3491c08f59bc
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-26 14:50:38 +0300

    Getting from session admin email

--- apis/createQuiz.php
+++ apis/createQuiz.php
@@ -2,7 +2,15 @@
 
 include '../config/config.php';
 
-$title = $_POST['title'] ?? '';
-$description = $_POST['description'] ?? '';
-
 //echo "Quiz: " . $title . " Created!";
+
+session_start();
+$is_admin = $_SESSION['is_admin'];
+if ($is_admin == 1) {
+    $currAdmin = $_SESSION['currAdmin'];
+    echo "Welcome Admin: " . $currAdmin;
+    $title = $_POST['title'] ?? '';
+    $description = $_POST['question'] ?? '';
+} else {
+    echo "You are not an admin!";
+}
