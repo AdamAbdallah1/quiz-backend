fb2fa95aa2211636cc2ea2eb5de9cbb67c3c2fdf
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-26 19:48:58 +0300

    Checking if admin and getting input

--- apis/questions/editQuestion.php
+++ apis/questions/editQuestion.php
@@ -9,3 +9,14 @@ if (!isset($_SESSION['user'])) {
 }
 
 $user = $_SESSION['user'];
+if ($user['is_admin'] == 1){
+    echo "Welcome Admin!\n";
+
+    $id = $_POST['id'] ?? 0;
+    $new_title = $_POST['title'] ?? '';
+    $new_description = $_POST['description'] ?? '';
+
+    if ($id, $new_title, $new_description) {
+        
+    }
+}
