a07b9512fa230fb52f2495c10b6168cd8c2606d1
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-26 16:03:18 +0300

    Cleaning code

--- apis/login.php
+++ apis/login.php
@@ -18,22 +18,20 @@ if ($email && $password) {
     
     if ($result->num_rows == 1){
         $user = $result->fetch_assoc();
+        $user = [
+            'id' => $user['id'],
+            'username' => $user['username'],
+            'email' => $user['email'],
+            'password' => $user['password'],
+            'is_admin' => $user['is_admin']
+        ];
+
+        $_SESSION['user'] = $user;
         if ($user['is_admin'] == 1) {
             echo "Welcome Admin: " . $user['username'];
-            $user = [
-                'id' => $user['id'],
-                'username' => $user['username'],
-                'email' => $user['email'],
-                'password' => $user['password'],
-                'is_admin' => $user['is_admin']
-            ];
-
-            $_SESSION['user'] = $user;
-        } else{
+                    } else{
             echo "Welcome User: " . $user['username'];
-            session_start();
-            $_SESSION['currUser'] = $user['email'];
-            $_SESSION['is_admin'] = 0;
+            
         }
     } else {
         echo "Invalid Email or Password";
